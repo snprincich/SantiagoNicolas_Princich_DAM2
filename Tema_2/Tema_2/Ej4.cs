e2cf9695178f14c1455144e7a3156b28caa176d8
Santiago Nicolas Princich <nico.princich@gmail.com>
2024-10-03 22:49:39 +0200

    Se crea el proyecto con la interfaz para ejecutar y el ejercicio4

---
+++ Tema_2/Tema_2/Ej4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_2
+{
+    internal class Ej4 : IEjecutarEjercicio
+    {
+        public void Ejecutar()
+        {
+
+        }
+    }
+}
---
+++ Tema_2/Tema_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_2
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            IEjecutarEjercicio ejercicio;
+            ejercicio = new Ej4();
+            ejercicio.Ejecutar();
+        }
+    }
+}
