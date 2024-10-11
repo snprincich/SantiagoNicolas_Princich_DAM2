590ec1ea3317fab70f8fb04a04e5ada25b912c6a
snprincich <nico.princich@gmail.com>
2024-10-11 18:39:54 +0200

    Se comienza el ejercicio 9

---
+++ Tema_2/Tema_2/Ej9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Haz una función que pueda tomar cualquier número entero no
+negativo como argumento y devolverlo con sus dígitos en orden
+descendente. Esencialmente, reordenar los dígitos para crear el
+mayor número posible.
+Entrada: 42145 Salida: 54421
+Entrada: 145263 Salida: 654321
+Entrada: 123456789 Salida: 987654321
+*/
+
+namespace Tema_2
+{
+    internal class Ej9
+    {
+    }
+}
