8bc17851198b67b58437fd7ca5713ac9db15d893
Santiago Nicolas Princich <nico.princich@gmail.com>
2024-10-13 21:32:52 +0200

    Ej10 Terminado

---
+++ Tema_2/Tema_2/Ej10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Escriba una función que tome un número decimal como entrada, y
+devuelva el número de bits que son iguales a uno en la
+representación binaria de ese número. Comprueba que la entrada no
+sea negativa.
+*/
+//La representación binaria de 1234 es 10011010010, por lo que la función
+//debería devolver 5 en este caso
+
+namespace Tema_2
+{
+    internal class Ej10 : IEjecutarEjercicio
+    {
+
+        public void Ejecutar()
+        {
+            int entrada = 1234;
+            Console.WriteLine($"El numero {entrada} en binario tiene {CantidadBitsUno(entrada)} unos");
+        }
+
+        private int CantidadBitsUno(int numero)
+        {
+            //CONVIERTE EL NUMERO A STRING, OPCIONALMENTE SE LE PUEDE AÑADIR LA BASE NUMERICA
+            String numeroBinario = Convert.ToString( numero, 2);
+            int cont = 0;
+
+            for (int i = 0; i < numeroBinario.Length; i++)
+            {
+                if (Char.GetNumericValue(numeroBinario[i]) == 1) cont++;
+            }
+            return cont;
+        }
+    }
+}
--- Tema_2/Tema_2/Program.cs
+++ Tema_2/Tema_2/Program.cs
@@ -8,7 +8,7 @@
             /*Utils prueba = new Utils();
             Console.WriteLine(prueba.EntradaSplitNumero());*/
 
-           ejercicio = new Ej9();
+           ejercicio = new Ej10();
             ejercicio.Ejecutar();
         }
     }
