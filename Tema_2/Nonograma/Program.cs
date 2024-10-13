bb27c741cbdfaba365f0904bb0f410a81bf88fbc
Santiago Nicolas Princich <nico.princich@gmail.com>
2024-10-13 21:01:22 +0200

    Se crean metodos en resolver. Todavia no se utilizan. Estan sin terminar

--- Tema_2/Nonograma/Program.cs
+++ Tema_2/Nonograma/Program.cs
@@ -7,25 +7,51 @@
         public static void Main(String[] args)
         {
             //int [,] columna = { {1,1,0},{4,0,0},{1,1,1}, {3,0,0}, {1,0,0} };
+            /*
             int[][] columna = new int[5][];
             columna[0] = [1, 1, 0];
             columna[1] = [4, 0, 0];
             columna[2] = [1, 1, 1];
             columna[3] = [3, 0, 0];
             columna[4] = [1,0,0];
-
-            Console.WriteLine(columna[0]);
-            
+*/
+            int[][] columna = new int[10][];
+            columna[0] = [1];
+            columna[1] = [1,3];
+            columna[2] = [1,4];
+            columna[3] = [4,2];
+            columna[4] = [5,1];
+            columna[5] = [5,1];
+            columna[6] = [4,2];
+            columna[7] = [4,1];
+            columna[8] = [2,2];
+            columna[9] = [3,4];
 
 
             //int[,] fila = { {1,0 }, {2,0}, {3,0}, {1,2}, {4,0} };
 
+            /*
             int[][] fila = new int[5][];
             fila[0] = [1,0];
             fila[1] = [2,0];
             fila[2] = [3,0];
             fila[3] = [1,2];
             fila[4] = [4,0];
+            */
+
+            int[][] fila = new int[10][];
+            fila[0] = [5];
+            fila[1] = [5,1];
+            fila[2] = [9];
+            fila[3] = [7];
+            fila[4] = [2];
+            fila[5] = [1];
+            fila[6] = [1,1,2,2];
+            fila[7] = [2,4];
+            fila[8] = [3,1];
+            fila[9] = [4];
+
+
 
             Nonograma consola = new Nonograma(fila, columna);
             consola.Mostrar();
--- Tema_2/Nonograma/Resolver.cs
+++ Tema_2/Nonograma/Resolver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Nonograma
 {
@@ -10,7 +11,7 @@ namespace Nonograma
     {
         Nonograma nonograma = Nonograma.ConsolaSingleInstance();
 
-        public int VacioEntreNumero(int[] array)
+        private int VacioEntreNumero(int[] array)
         {
             List<int> numeros = new List<int>();
             foreach (var item in array)
@@ -31,7 +32,7 @@ namespace Nonograma
             int numero = array.Sum();
             int cont = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < nonograma.Fila.Length; i++)
             {
                 if (nonograma.Solucion[i, posicion] == 1) cont++;
             }
@@ -51,7 +52,7 @@ namespace Nonograma
             int numero = array.Sum();
             int cont = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < nonograma.Columna.Length; i++)
             {
                 if (nonograma.Solucion[posicion, i] == 1) cont++;
             }
@@ -60,7 +61,9 @@ namespace Nonograma
                 for (int i = 0; i < nonograma.FilaCompletados[posicion].Length; i++)
                 {
                     nonograma.FilaCompletados[posicion][i] = true;
+
                 }
+
                 return true;
             }
             return false;
@@ -153,10 +156,13 @@ namespace Nonograma
 
             for (int i = 0; i < nonograma.Fila.Length; i++)
             {
+
                 if (EstaCompletadoFila(nonograma.Fila[i], i))
                 {
+
                     for (int b = 0; b < nonograma.Columna.Length; b++)
                     {
+
                         if (nonograma.Solucion[i, b] == 0) nonograma.Solucion[i, b] = 2;
                     }
                 }
@@ -169,26 +175,185 @@ namespace Nonograma
 
 
         //COMPRUEBA SI UNA PISTA ESTA TERMINADA
-        public void ComprobarTerminadaIndividual()
+        //SOLO FILAS
+        //PARAMETROS ARRAY DE PISTAS (FILA) Y POSICION DE LA FILA
+        public void ComprobarTerminadaIndividual(int[] fila, int posicion)
         {
+            List<int[]> posiciones = PosiblesPosicionesFila(fila, posicion);
+            //RECORRE LAS PISTAS
+            for (int i = 0; i < fila.Length; i++)
+            {
+                //RECORRE LAS CASILLAS
+                for (int b = 0; b < nonograma.Columna.Length; b++)
+                {
+                    //SI ENCONTRAMOS UNA CASILLA PINTADA 
+                    if (nonograma.Solucion[posicion, b] == 1)
+                    {
+                        //SE COMPRUEBA QUE ESTE DENTRO DE LA POSICION MINIMA Y MAXIMA DE LA PISTA
+                        if (posiciones[i].Min() <= b && posiciones[i].Max() >= b)
+                        {
+                            //COMPRUEBA QUE A PARTIR DEL INDICE B HAYA CASILLAS PINTADAS EQUIVALENTE A LA PISTA
+                            if (ComprobarCasillasAlrededor(posicion, fila[i], b))
+                            {
+                                //SE CREA UN ARRAY CON LAS PISTAS POSTERIORES A LA ACTUAL
+                                int[] arrayCortado = new int[(fila.Length - i)-1];
+                                Array.Copy(fila, i + 1, arrayCortado, 0, ((fila.Length - i) - 1));
 
+                                //CUANTO ESPACIO NECESITAMOS PARA LAS PISTAS + 1 (ESPACIO PROHIBIDO ENTRE PISTA Y PISTA)
+                                int ocupa = Ocupa(arrayCortado) + 1;
+
+                                //HAY HUECO SOLO COMPRUEBA HACIA LA IZQUIERDA
+                                if (HayHueco(CrearArrayUnidimensionalFila(posicion), ocupa, arrayCortado))
+                                {
+                                    nonograma.FilaCompletados[posicion][i] = true;
+                                }
+
+
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //COMPRUEBA SI HAY CASILLAS SUFICIENTES PARA QUE ENTREN LAS PISTAS QUE ENTRAN POR PARAMETRO
+        //QUIZAS DA EXCEPCION POR SALIRSE DEL ARRAY
+        public bool HayHueco(int[] array, int ocupa, int[] pistas)
+        {
+            int cont = 0;
+            int pistaIndice = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 2)
+                {
+
+                    cont++;
+                    for (int b = 1; b < pistas[pistaIndice]; b++)
+                    {
+                        if (array[i+b]!=2) cont++;
+                        else
+                        {
+                            i = i + b + 1;
+                            cont = 0;
+                        }
+                    }
+                    if (cont == pistas[pistaIndice])
+                    {
+                        i = pistas[pistaIndice] + 1;
+                        pistaIndice++;
+                        cont = 0;
+                    }
+
+                }
+            }
+
+            if (pistaIndice == pistas.Length) return true;
+
+            return false;
+        }
+
+        //DEBERIA FUNCIONAR
+        //COMPRUEBA SI HAY CASILLAS PINTADAS EQUIVALENTES A LA PRUEBA ALREDEDOR DEL INDICE 
+        //POSICION (FILA) DEL TABLERO PINTADO
+        //PISTA A BUSCAR
+        //INDICE DE LA CASILLA
+        public Boolean ComprobarCasillasAlrededor(int array, int pista, int indice)
+        {
+            //HA ENCUENTRA LA PISTA COMPLETA A PARTIR DE ESE INDICE?
+            bool encontrado = false;
+
+            //CUANTOS PINTADOS ENCUENTRA INMEDIATAMENTE ANTES Y DESPUES DEL INDICE
+            int contIzq = 0;
+            int contDer = 0;
+
+            //SEGUN QUE TAN GRANDE SEA LA PISTA SE MUEVE MAS O MENOS CASILLAS
+            for (int b = 1; b < pista; b++)
+            {
+                //SI LA PISTA ENTRA ENTRE EL INDICE Y PISTA-1 POSICIONES A LA IZQUIERDA (EVITAR SALIRSE DEL RANGO DEL ARRAY) y B POSICIONES HACIA ATRAS HAY UN 1
+                if ((indice - (pista - 1) > -1) && (nonograma.Solucion[array, indice - b] == 1))
+                {
+                    contIzq++;
+                }
+                //SI LA PISTA ENTRA ENTRE EL INDICE Y PISTA+1 POSICIONES A LA DERECHA (EVITAR SALIRSE DEL RANGO DEL ARRAY) y B POSICIONES HACIA ADELANTE HAY UN 1
+                if ((indice + (pista - 1) < nonograma.Columna.Length) && (nonograma.Solucion[array, indice + b] == 1))
+                {
+                    contDer++;
+                }
+            }
+
+            //SI HA ENCONTRADO A LA IZQUIERDA y A LA DERECHA SUFICIENTE PINTADOS PARA FORMAR LA PISTA ES VERDADERO
+            //SI HAY MAS, LAS CASILLAS NO LE PERTENECEN A LA PISTA
+            //SI HAY MENOS PUEDE SER QUE LE PERTENEZCAN
+            if ((contIzq == pista - 1 || contDer == pista - 1) && (contIzq + contDer + 1 == pista))
+            {
+                encontrado = true;
+            }
+
+
+
+            return encontrado;
+        }
+
+        //DE CADA PISTA TE DICE LA POSICION MINIMA Y MAXIMA EN LA QUE PUEDE ESTAR
+        //DEVUELVE UNA LISTA DE ARRAYS CON LA POSICION MINIMA Y MAXIMA CORRESPONDIENTE
+        public List<int[]> PosiblesPosicionesFila(int[] array, int posicion)
+        {
+            List<int[]> posiciones = new List<int[]>();
+            int inicio;
+            int final;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                inicio = EmpiezaEn(array, i);
+                final = inicio + (nonograma.Columna.Length - Ocupa(array));
+                posiciones.Add([inicio, final]);
+            }
+
+            return posiciones;
+        }
+
+        public int[] CrearArrayUnidimensionalFila(int posicion)
+        {
+            int[] array = new int[nonograma.Columna.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = nonograma.Solucion[posicion,i];
+            }
+            return array;
+        }
+
+        public List<int[]> PosiblesPosicionesColumna(int[] array, int posicion)
+        {
+            List<int[]> posiciones = new List<int[]>();
+            int inicio;
+            int final;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                inicio = EmpiezaEn(array, i);
+                final = inicio + (nonograma.Fila.Length - Ocupa(array));
+                posiciones.Add([inicio, final]);
+            }
+
+            return posiciones;
         }
 
         //SIN TERMINAR
         public void SegundaPasada()
         {
-            
+
 
             for (int i = 0; i < nonograma.Fila.Length; i++)
             {
                 List<int> imposibles = VerImposibles(i);
 
-                
+
                 for (int j = 1; j < imposibles.Count; j++)
                 {
                     //LA DIFERENCIA ENTRE DOS NUMEROS ES LA RESTA DE AMBOS PASADA A VALOR ABSOLUTO
-                    int hueco = Math.Abs((imposibles[j]-1) - imposibles [j]);
-                    
+                    int hueco = Math.Abs((imposibles[j] - 1) - imposibles[j]);
+
                     for (int k = 1; k < nonograma.Fila.Length; k++)
                     {
                         if (hueco >= nonograma.Fila[i][k])
@@ -206,7 +371,7 @@ namespace Nonograma
             List<int> posiciones = new List<int>();
             posiciones.Add(-1);
 
-            
+
             for (int i = 0; i < nonograma.Fila.Length; i++)
             {
 
@@ -222,7 +387,7 @@ namespace Nonograma
             PrimeraPasada();
             ComprobarTerminadas();
 
-            
+
             /*while (true)
             {
 
