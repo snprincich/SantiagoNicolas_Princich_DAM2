f518509d50435967d7addfcbdb04026d6860718c
snprincich <nico.princich@gmail.com>
2025-01-14 21:41:25 +0100

    Se separa el metodo de autorizacion

--- BasicApp/PokeRogue/Utils/HttpJsonClient.cs
+++ BasicApp/PokeRogue/Utils/HttpJsonClient.cs
@@ -7,13 +7,14 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
 using BasicApp.DTO;
+using System.Security.Policy;
 
 namespace BasicApp.Utils
 {
     public static class HttpJsonClient<T>
     {
         public static string Token = string.Empty;
-
+        
         //CREAR UN METODO SOLO PARA LA RENOVACION DE TOKEN ASI NO HAY QUE ESCRIBIRLO EN EL GET, POST Y PATCH
         public static async Task<T?> Get(string path)
         {
@@ -25,21 +26,7 @@ namespace BasicApp.Utils
                     HttpResponseMessage request = await httpClient.GetAsync($"{Constantes.BASE_URL}{path}");
                     if (request.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                     {
-                        LoginDTO loginDTO = new LoginDTO
-                        {
-                            Password = "wnD/LbJq?9t,}-628%)",
-                            UserName = "sufrido"
-                        };
-                        HttpContent httpContent = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "application/json");
-
-                        HttpResponseMessage requestToken = await httpClient.PostAsync($"{Constantes.BASE_URL}{Constantes.LOGIN_PATH}/login", httpContent);
-
-                        string dataTokenRequest = await requestToken.Content.ReadAsStringAsync();
-                        UserDTO tokenUser = JsonSerializer.Deserialize<UserDTO>(dataTokenRequest);
-
-                        Token = tokenUser?.Result?.Token ?? string.Empty;
-                        httpClient.DefaultRequestHeaders.Remove("Authorization");
-                        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Token}");
+                        await Authenticate(path, httpClient, request);
                         request = await httpClient.GetAsync($"{Constantes.BASE_URL}{path}");
                     }
                     string dataRequest = await request.Content.ReadAsStringAsync();
@@ -53,33 +40,63 @@ namespace BasicApp.Utils
             return default;
         }
 
+        private static async Task Authenticate(string path, HttpClient httpClient, HttpResponseMessage request)
+        {
+            LoginDTO loginDTO = new LoginDTO
+            {
+                Password = "wnD/LbJq?9t,}-628%)",
+                UserName = "sufrido"
+            };
+            HttpContent httpContent = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "application/json");
 
-        public static async Task<object> Post(string url, object data)
+            HttpResponseMessage requestToken = await httpClient.PostAsync($"{Constantes.BASE_URL}{Constantes.LOGIN_PATH}/login", httpContent);
+
+            string dataTokenRequest = await requestToken.Content.ReadAsStringAsync();
+            UserDTO tokenUser = JsonSerializer.Deserialize<UserDTO>(dataTokenRequest);
+
+            Token = tokenUser?.Result?.Token ?? string.Empty;
+            httpClient.DefaultRequestHeaders.Remove("Authorization");
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Token}");
+        }
+
+        public static async Task<object> Post(string path, object data)
         {
             try
             {
                 using (HttpClient httpClient = new HttpClient())
                 {
-                    // Serializar el objeto 'data' (PokemonDTO) a JSON
-                    string jsonContent = JsonSerializer.Serialize(data);
 
-                    // Crear el contenido HTTP con el tipo adecuado para enviar JSON
-                    var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-                    // Realizar la solicitud POST
-                    HttpResponseMessage response = await httpClient.PostAsync(url, content);
-
-                    // Verificar si la respuesta fue exitosa
-                    if (response.IsSuccessStatusCode)
+                    httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Token}");
+                    HttpResponseMessage request = await httpClient.GetAsync($"{Constantes.BASE_URL}{path}");
+                    if (request.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                     {
-                        // Leer el contenido de la respuesta y deserializarlo
-                        string responseBody = await response.Content.ReadAsStringAsync();
-                        return JsonSerializer.Deserialize<T>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                        await Authenticate(path, httpClient, request);
+
+                        // Serializar el objeto 'data' (PokemonDTO) a JSON
+                        string jsonContent = JsonSerializer.Serialize(data);
+
+                        // Crear el contenido HTTP con el tipo adecuado para enviar JSON
+                        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                        // Realizar la solicitud POST
+                        HttpResponseMessage response = await httpClient.PostAsync(url, content);
+
+                        request = await httpClient.GetAsync($"{Constantes.BASE_URL}{path}");
+
+                        // Verificar si la respuesta fue exitosa
+                        if (response.IsSuccessStatusCode)
+                        {
+                            // Leer el contenido de la respuesta y deserializarlo
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            return JsonSerializer.Deserialize<T>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error en la respuesta: " + response.StatusCode);
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine("Error en la respuesta: " + response.StatusCode);
-                    }
+                    string dataRequest = await request.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<T>(dataRequest);
                 }
             }
             catch (Exception ex)
